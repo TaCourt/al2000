ab4ec74dc3833be9892f27380f4314aadf2ab59c
mathieugautron <mathieuga23@gmail.com>
2019-11-18 14:19:06 +0100

    Ajout des méthodes addMovie et removeMovie dans la classe FilmLibrary

--- src/core/FilmLibrary.java
+++ src/core/FilmLibrary.java
@@ -1,23 +1,57 @@
 package src.core;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 
 public class FilmLibrary{
-    private List<Movie> cyberVideoMovies;
-    private List<Movie> availableMovies;
-    private int nbExemplaireFilm;
+    private Map<UUID, Movie> cyberVideoMovies;
+    private Map<UUID, Movie> al2000Movies;
+    private Map<UUID, Movie> availableMovies;
+    private Map<UUID, Integer> nbMovieCopies;
 
-    public FilmLibrary(List<Movie> cyberVideoMovie, List<Movie> availableMovies, int nbExemplaireFilm){
+    public FilmLibrary(HashMap<UUID, Movie> cyberVideoMovie, HashMap<UUID, Movie> availableMovies, HashMap<UUID, Movie> al2000Movies, int nbExemplaireFilm){
         this.cyberVideoMovies = cyberVideoMovie;
         this.availableMovies = availableMovies;
-        this.nbExemplaireFilm = nbExemplaireFilm;
+        this.al2000Movies = al2000Movies;
     }
 
-    public List<Movie> getCyberVideoMovies() {
+    public Map<UUID, Movie> getCyberVideoMovies() {
         return this.cyberVideoMovies;
     }
 
-    public List<Movie> getAvailableMovies() {
+    public Map<UUID, Movie> getAl2000Movies() {
+        return this.al2000Movies;
+    }
+
+    public Map<UUID, Movie> getAvailableMovies() {
         return this.availableMovies;
     }
+
+    public void addMovie(Movie m) {
+        Integer nbCopies = nbMovieCopies.get(m.getMovieID());
+        if (nbCopies == 0) {
+            availableMovies.put(m.getMovieID(), m);
+        }
+        nbCopies++;
+
+    }
+
+    public boolean removeMovie(Movie m) {
+        Integer nbCopies = nbMovieCopies.get(m.getMovieID());
+        if (nbCopies < 1) {
+            return false;
+        }
+        nbMovieCopies.get(m.getMovieID());
+        nbCopies--;
+        if (nbCopies == 0) {
+            availableMovies.remove(m.getMovieID());
+        }
+        return true;
+    }
+
+
+
+
 }
--- src/core/Movie.java
+++ src/core/Movie.java
@@ -1,7 +1,9 @@
 package src.core;
 
 
-public class Movie{
+import java.util.UUID;
+
+public class Movie implements Cloneable {
     private String affiche;
     private String titre;
     private String categorie;
@@ -11,6 +13,7 @@ public class Movie{
     //private ...... liste;
     private String acteurs;
     private String realisateurs;
+    private UUID movieID;
 
     public Movie(String affiche, String titre, String categorie, String synopsis, int duree, String langue, String acteurs, String realisateurs){
         this.affiche = affiche;
@@ -21,5 +24,10 @@ public class Movie{
         this.langue = langue;
         this.acteurs = acteurs;
         this.realisateurs = realisateurs;
+        this.movieID = UUID.randomUUID();
+    }
+
+    public UUID getMovieID() {
+        return this.movieID;
     }
 }
