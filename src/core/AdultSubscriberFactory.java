81965697758c4928fce2178d40b8f37540341008
mathieugautron <mathieuga23@gmail.com>
2019-11-21 23:00:57 +0100

    Changement de conception : suppression de User et de lambdaUser

--- src/core/AdultSubscriberFactory.java
+++ src/core/AdultSubscriberFactory.java
@@ -2,13 +2,13 @@ package src.core;
 
 import java.util.UUID;
 
-public class AdultSubscriberFactory extends UserFactory {
+public class AdultSubscriberFactory extends SubscriberFactory {
 
     @Override
-    public User makeUser() {
+    public Subscriber makeUser() {
         return new AdultSubscriber(UUID.randomUUID(), 0, "", "", 0);
     }
-    public User makeUser(String lastName, String firstName, String creditCard){
+    public Subscriber makeUser(String lastName, String firstName, String creditCard){
         return new AdultSubscriber(UUID.randomUUID(),Long.parseLong(creditCard),lastName,firstName,0);
     }
 }
--- src/core/ChildSubscriberFactory.java
+++ src/core/ChildSubscriberFactory.java
@@ -1,17 +1,15 @@
 package src.core;
 
-import src.core.ChildSubscriber;
-
 import java.util.UUID;
 
-public class ChildSubscriberFactory extends UserFactory {
+public class ChildSubscriberFactory extends SubscriberFactory {
 
-    public User makeUser(String lastName, String firstName, AdultSubscriber adultSub){
+    public Subscriber makeUser(String lastName, String firstName, AdultSubscriber adultSub){
         return new ChildSubscriber(UUID.randomUUID(),adultSub.getCreditCard(),lastName,firstName,0,adultSub);
     }
 
     @Override
-    public User makeUser() {
+    public Subscriber makeUser() {
         return null;
     }
 }
