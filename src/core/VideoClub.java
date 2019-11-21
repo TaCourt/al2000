22fbf875fe417f9b7a5510df7a8f63614520440e
Florian Cappa <cflo84@gmail.com>
2019-11-21 20:30:59 +0100

    Refactor : moved save/load functions from VideoClub to a DAO

--- src/core/VideoClub.java
+++ src/core/VideoClub.java
@@ -1,27 +1,16 @@
 package src.core;
 
 import java.util.List;
-import org.json.simple.parser.ParseException;
 import src.gui.ConsoleUserInterface;
-import src.persistence.JSONPersistence;
 import src.persistence.Persistence;
 import src.gui.UserInterface;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 public class VideoClub {
-    public enum ErrorState {DB_NOT_LOADED};
-    private ErrorState errorState = null;
-
-    private Persistence persistence;
+    private DAO dao;
     private UserInterface gui;
-
-    private final String ADULT_SUBSCRIBER_ACCOUNT = "adultSubscriber";
-    private final String CHILD_SUBSCRIBER_ACCOUNT = "childSubscriber";
 
     private List<Technician> technicians;
     private UserFactory factory;
@@ -31,169 +20,19 @@ public class VideoClub {
     private int fineCost;
 
     public VideoClub() {
-        try {
-            this.persistence = new JSONPersistence();
-        } catch (IOException | ParseException e) {
-            this.errorState = ErrorState.DB_NOT_LOADED;
-            System.out.println(e);
-        }
-
         gui = new ConsoleUserInterface(this);
+    }
+    public void setPersistence(Persistence persistence) {
+        this.dao = new DAO();
+        this.dao.setPersistence(persistence);
     }
 
     public void launch() {
 
     }
 
-    private void save(Subscriber subscriber, HashMap<String, String> subscriberDetails) {
-        final StringJoiner joinedCategoryRestrained = new StringJoiner(",");
-        subscriber.getCategoryRestrained().forEach((String category) -> {
-            joinedCategoryRestrained.add(category);
-        });
-
-        final StringJoiner joinedCurrentRentedMovies = new StringJoiner(",");
-        subscriber.getCurrentRentedMovies().forEach((Rental rental) -> {
-            joinedCurrentRentedMovies.add(rental.getRentalId().toString());
-        });
-
-        final StringJoiner joinedHistory = new StringJoiner(",");
-        subscriber.getHistory().forEach((Rental rental) -> {
-            joinedHistory.add(rental.getRentalId().toString());
-        });
-
-        final StringJoiner joinedMoviesRestrained = new StringJoiner(",");
-        subscriber.getMoviesRestrained().forEach((String movieTitle) -> {
-            joinedMoviesRestrained.add(movieTitle);
-        });
-
-        subscriberDetails.put("name", subscriber.getName());
-        subscriberDetails.put("firstName", subscriber.getFirstName());
-        subscriberDetails.put("creditCard", Long.toString(subscriber.getCreditCard()));
-        subscriberDetails.put("balanceSubscriberCard", Double.toString(subscriber.getBalanceSubscriberCard()));
-        subscriberDetails.put("categoryRestrained", joinedCategoryRestrained.toString());
-        subscriberDetails.put("currentRentedMovies", joinedCurrentRentedMovies.toString());
-        subscriberDetails.put("history", joinedHistory.toString());
-        subscriberDetails.put("moviesRestrained", joinedMoviesRestrained.toString());
-        subscriberDetails.put("maxMovieRented", Integer.toString(subscriber.getMaxMovieRented()));
-        subscriberDetails.put("UUID", subscriber.getSubscriberId().toString());
-
-        this.persistence.saveSubscriber(subscriberDetails);
-    }
-
-    public void save(AdultSubscriber adultSubscriber) {
-        HashMap<String, String> subscriberDetails = new HashMap<String, String>();
-
-        final StringJoiner childrenIds = new StringJoiner(",");
-        adultSubscriber.getChildren().forEach((ChildSubscriber child) -> {
-            childrenIds.add(child.getSubscriberId().toString());
-        });
-
-        subscriberDetails.put("account", this.ADULT_SUBSCRIBER_ACCOUNT);
-        subscriberDetails.put("children", childrenIds.toString());
-
-        this.save(adultSubscriber, subscriberDetails);
-    }
-
-    public void save(ChildSubscriber childSubscriber) {
-        HashMap<String, String> subscriberDetails = new HashMap<String, String>();
-
-        subscriberDetails.put("account", this.CHILD_SUBSCRIBER_ACCOUNT);
-        subscriberDetails.put("parent", childSubscriber.getParent().getSubscriberId().toString());
-
-        this.save(childSubscriber, subscriberDetails);
-    }
-
-    public Subscriber loadSubscriber(String id) {
-        HashMap<String, String> subscriberDetails = this.persistence.loadSubscriber(id);
-
-        if (subscriberDetails == null) {
-            return null;
-        }
-        Subscriber subscriber = null;
-        String account = subscriberDetails.get("account");
-
-        if (this.ADULT_SUBSCRIBER_ACCOUNT.equals(account)) {
-            subscriber = this.loadAdultSubscriber(subscriberDetails);
-        } else if (this.CHILD_SUBSCRIBER_ACCOUNT.equals(account)) {
-            ChildSubscriber childSubscriber = this.loadChildSubscriber(subscriberDetails);
-            loadSubscriber(subscriberDetails, childSubscriber);
-
-            subscriber = childSubscriber;
-        }
-
-        return subscriber;
-    }
-
-    private ChildSubscriber loadChildSubscriber(HashMap<String, String> subscriberDetails) {
-        return loadChildSubscriber(subscriberDetails, this.loadAdultSubscriber(subscriberDetails));
-    }
-
-    private ChildSubscriber loadChildSubscriber(HashMap<String, String> subscriberDetails, AdultSubscriber parent) {
-        String childId = subscriberDetails.get("UUID");
-
-        ChildSubscriber childSubscriber = new ChildSubscriber(UUID.fromString(childId),
-                Long.parseLong(subscriberDetails.get("creditCard")),
-                subscriberDetails.get("name"),
-                subscriberDetails.get("firstName"),
-                Double.parseDouble(subscriberDetails.get("balanceSubscriberCard")),
-                parent);
-        return childSubscriber;
-    }
-
-    private AdultSubscriber loadAdultSubscriber(HashMap<String, String> subscriberDetails) {
-        final String adultSubscriberId = subscriberDetails.get("UUID");
-
-        AdultSubscriber adultSubscriber = new AdultSubscriber(UUID.fromString(adultSubscriberId),
-                Long.parseLong(subscriberDetails.get("creditCard")),
-                subscriberDetails.get("name"),
-                subscriberDetails.get("firstName"),
-                Double.parseDouble(subscriberDetails.get("balanceSubscriberCard")));
-
-        String[] splitString = subscriberDetails.get("children").split(",");
-        for (String childId : splitString) {
-            adultSubscriber.addChild(this.loadChildSubscriber(this.persistence.loadSubscriber(childId), adultSubscriber));
-        }
-        /*this.persistence.forEachSubscriber((_id, _userDetails) -> {
-            if ((_userDetails).get("account").equals(this.CHILD_SUBSCRIBER_ACCOUNT) && ((_userDetails).get("parent").equals(adultSubscriberId))) {
-                adultSubscriber.addChild(this.loadChildSubscriber(_userDetails, adultSubscriber));
-            }
-        });*/
-
-        loadSubscriber(subscriberDetails, adultSubscriber);
-        return adultSubscriber;
-    }
-
-    private void loadSubscriber(HashMap<String, String> subscriberDetails, Subscriber subscriber) {
-        String[] splitString;
-        splitString = subscriberDetails.get("categoryRestrained").split(",");
-        for (String category : splitString) {
-            subscriber.restrainMovieByTitle(category);
-        }
-
-        splitString = subscriberDetails.get("currentRentedMovies").split(",");
-        for (String currentRentalId : splitString) {
-            subscriber.addRental(this.loadRental(currentRentalId));
-        }
-
-        splitString = subscriberDetails.get("history").split(",");
-        for (String oldRentalId : splitString) {
-            subscriber.addRental(this.loadRental(oldRentalId));
-        }
-
-        splitString = subscriberDetails.get("moviesRestrained").split(",");
-        for (String movieRestrainedTitle : splitString) {
-            subscriber.restrainMovieByTitle(movieRestrainedTitle);
-        }
-    }
-
-    private Movie loadMovie(String movieRestrainedId) {
-        // TODO
-        return null;
-    }
-
-    private Rental loadRental(String currentRentalId) {
-        // TODO
-        return null;
+    public void logIn (String subscriberId) {
+        this.currentSubscriber = this.dao.loadSubscriber(subscriberId);
     }
 
     public void addTechnicians(Technician technician) {
@@ -368,12 +207,12 @@ public class VideoClub {
     public void createNewSubscriber(String[] userData) {
         AdultSubscriberFactory userFactory = new AdultSubscriberFactory();
         User user = userFactory.makeUser(userData[0], userData[1], userData[2]);
-        save((AdultSubscriber) user);
+        this.dao.save((AdultSubscriber) user);
     }
 
     public void createNewChildSubscriber(String[] userData) {
         ChildSubscriberFactory userFactory = new ChildSubscriberFactory();
         User user = userFactory.makeUser(userData[0], userData[1], (AdultSubscriber) currentSubscriber);
-        save((AdultSubscriber) user);
+        this.dao.save((AdultSubscriber) user);
     }
 }
