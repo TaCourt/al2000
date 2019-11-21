da6166885ae567c3429daa60eddb469f5a94929f
Florian Cappa <cflo84@gmail.com>
2019-11-21 21:30:21 +0100

    Added movie save method

--- src/persistence/Persistence.java
+++ src/persistence/Persistence.java
@@ -8,9 +8,14 @@ import java.util.HashMap;
 import java.util.function.BiConsumer;
 
 public interface Persistence {
-    void saveSubscriber(HashMap<String, String> userDetails);
+    void saveSubscriber (HashMap<String, String> userDetails);
+    void saveMovie (HashMap<String, String> movieDetails);
+    void saveRental (HashMap<String, String> movieDetails);
 
     HashMap<String, String> loadSubscriber(String id);
+    HashMap<String, String> loadMovie(String id);
+    HashMap<String, String> loadRental(String id);
 
-    void forEachSubscriber(BiConsumer<String, HashMap<String, String>> callback);
+    void forEachSubscriber (BiConsumer<String, HashMap<String, String>> callback);
+
 }
